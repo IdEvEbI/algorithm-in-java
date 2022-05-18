fb26816ede44d9b3c6700b9c79637339bd3e0033
壹刀流 <idevebi@163.com>
2022-05-18 18:46:30 +0800

    feat: leetcode 206 reverse linked list

---
+++ src/com/idevebi/algorithms/datastructures/linkedList/Leetcode206ReverseLinkedList.java
@@ -0,0 +1,56 @@
+package com.idevebi.algorithms.datastructures.linkedList;
+
+/**
+ * 反转链表
+ *
+ * <a href="https://leetcode.cn/problems/reverse-linked-list/">https://leetcode.cn/problems/reverse-linked-list/</a>
+ */
+public class Leetcode206ReverseLinkedList {
+    public static void main(String[] args) {
+        // 创建链表
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+
+        head = reverseList(head);
+
+        System.out.println(head);
+    }
+
+    /**
+     * 给定单链表的头节点 head，反转链表并返回反转后的链表 head。
+     *
+     * @param head 头节点
+     * @return 翻转后的头节点
+     */
+    private static ListNode reverseList(ListNode head) {
+        // 反转后的头指针
+        ListNode rHead = null;
+
+        // 当前头指针
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+
+            cur.next = rHead;
+            rHead = cur;
+            cur = next;
+        }
+
+        return rHead;
+    }
+}
+
+/**
+ * 链表节点
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
