6e80bdaf2a8c28d3b8d257cf9558f95dfa91ada0
壹刀流 <idevebi@163.com>
2022-05-25 17:52:19 +0800

    feat: leetcode [1137] nth tribonacci number

--- src/com/idevebi/leetcode/editor/cn/FibonacciNumber.java
+++ src/com/idevebi/leetcode/editor/cn/FibonacciNumber.java
@@ -2,7 +2,7 @@ package com.idevebi.leetcode.editor.cn;
 
 /**
  * 509. 斐波那契数
- *
+ * <p>
  * 题目链接：<a href=" https://leetcode.cn/problems/fibonacci-number"> https://leetcode.cn/problems/fibonacci-number</a>/
  */
 public class FibonacciNumber {
@@ -17,28 +17,28 @@ public class FibonacciNumber {
         System.out.println(solution.fib(0));
     }
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int fib(int n) {
-        if (n < 2) {
-            return n;
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int fib(int n) {
+            if (n < 2) {
+                return n;
+            }
+
+            // F(n) = F(n-1) + F(n-2)
+            int ans = 1;
+            // F(n-1) 和 F(n-2)
+            int last = 1, preLast = 1;
+
+            for (int i = 2; i < n; i++) {
+                ans = last + preLast;
+
+                preLast = last;
+                last = ans;
+            }
+
+            return ans;
         }
-
-        // F(n) = F(n-1) + F(n-2)
-        int ans = 1;
-        // F(n-1) 和 F(n-2)
-        int last = 1, preLast = 1;
-
-        for (int i = 2; i < n; i++) {
-            ans = last + preLast;
-
-            preLast = last;
-            last = ans;
-        }
-
-        return ans;
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
---
+++ src/com/idevebi/leetcode/editor/cn/NThTribonacciNumber.java
@@ -0,0 +1,45 @@
+package com.idevebi.leetcode.editor.cn;
+
+/**
+ * 1137. 第 N 个泰波那契数
+ * <p>
+ * 题目链接：<a href="https://leetcode.cn/problems/n-th-tribonacci-number/">https://leetcode.cn/problems/n-th-tribonacci-number/</a>
+ */
+public class NThTribonacciNumber {
+    public static void main(String[] args) {
+        Solution solution = new NThTribonacciNumber().new Solution();
+
+        System.out.println(solution.tribonacci(4));
+        System.out.println(solution.tribonacci(25));
+        System.out.println(solution.tribonacci(3));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int tribonacci(int n) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n <= 2) {
+                return 1;
+            }
+
+            // F(n) = F(n-1) + F(n-2) + F(n-3)
+            int ans = 2;
+            // F(n-1) 和 F(n-2) 和 F(n-3)
+            int last = 1, preLast = 1, prePreLast = 0;
+
+            for (int i = 3; i <= n; i++) {
+                ans = last + preLast + prePreLast;
+
+                prePreLast = preLast;
+                preLast = last;
+                last = ans;
+            }
+
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
