1f10604a7c81a0920a534ea86870a68b0741f4ee
壹刀流 <idevebi@163.com>
2022-05-26 16:42:17 +0800

    feat: leetcode [701] insert into BST

---
+++ src/com/idevebi/leetcode/editor/cn/Leetcode701InsertIntoABinarySearchTree.java
@@ -0,0 +1,74 @@
+package com.idevebi.leetcode.editor.cn;
+
+import com.idevebi.leetcode.editor.cn.datastructures.tree.TreeNode;
+
+public class Leetcode701InsertIntoABinarySearchTree {
+    public static void main(String[] args) {
+        /*
+         *                5
+         *             3     7
+         *           1   4  6  9
+         */
+        // 创建二叉树
+        TreeNode root = new TreeNode(5);
+
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(4);
+
+        root.right = new TreeNode(7);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        Solution solution = new Leetcode701InsertIntoABinarySearchTree().new Solution();
+        solution.insertIntoBST(root, 2);
+
+        // 测试连续插入 1~5 观察二叉树退化成链表
+        root = solution.insertIntoBST(null, 1);
+        for (int i = 2; i < 6; i++) {
+            solution.insertIntoBST(root, i);
+        }
+        System.out.println("二叉树退化");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    class Solution {
+        public TreeNode insertIntoBST(TreeNode root, int val) {
+            // 新建节点
+            TreeNode n = new TreeNode(val);
+
+            // 如果 root 为 null，直接创建节点
+            if (root == null) {
+                return n;
+            }
+
+            // 遍历节点
+            TreeNode cur = root;
+            while (cur != null) {
+                // 如果 val 比 cur.val 小，查找左侧节点
+                if (val < cur.val) {
+                    // 如果左侧叶子节点为空，直接添加节点
+                    if (cur.left == null) {
+                        cur.left = n;
+                        break;
+                    } else {
+                        cur = cur.left;
+                    }
+                } else {
+                    if (cur.right == null) {
+                        cur.right = n;
+                        break;
+                    } else {
+                        cur = cur.right;
+                    }
+                }
+            }
+
+            // 返回根节点
+            return root;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
