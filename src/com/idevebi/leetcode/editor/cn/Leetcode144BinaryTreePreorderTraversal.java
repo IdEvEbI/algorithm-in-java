3e8f955470f937b0f5d28326af58ee8d7c3e166f
壹刀流 <idevebi@163.com>
2022-05-27 10:31:23 +0800

    refactor: leetcode [144] b tree preorder with stack

--- src/com/idevebi/leetcode/editor/cn/Leetcode144BinaryTreePreorderTraversal.java
+++ src/com/idevebi/leetcode/editor/cn/Leetcode144BinaryTreePreorderTraversal.java
@@ -4,6 +4,8 @@ import com.idevebi.leetcode.editor.cn.datastructures.tree.TreeNode;
 import com.idevebi.leetcode.editor.cn.util.TreeHelper;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Leetcode144BinaryTreePreorderTraversal {
@@ -20,7 +22,41 @@ public class Leetcode144BinaryTreePreorderTraversal {
     //leetcode submit region begin(Prohibit modification and deletion)
 
     class Solution {
+
+        /**
+         * 使用 stack 实现前序遍历
+         */
         public List<Integer> preorderTraversal(TreeNode root) {
+            // 遍历结果
+            List<Integer> res = new ArrayList<>();
+
+            // 判断根节点
+            if (root == null) {
+                return res;
+            }
+
+            Deque<TreeNode> stack = new LinkedList<>();
+            TreeNode n = root;
+
+            while (!stack.isEmpty() || n != null) {
+                // 根 + 左
+                while (n != null) {
+                    res.add(n.val);
+                    stack.push(n);
+                    n = n.left;
+                }
+                // 右
+                n = stack.pop();
+                n = n.right;
+            }
+
+            return res;
+        }
+
+        /**
+         * 使用递归实现前序遍历
+         */
+        public List<Integer> preorderTraversal2(TreeNode root) {
             // 遍历结果
             List<Integer> res = new ArrayList<>();
 
