02dfb8a636992a3630436f4c3014c404383c8ee8
壹刀流 <idevebi@163.com>
2022-05-29 12:43:29 +0800

    refactor: leetcode [50] pow x n

---
+++ src/com/idevebi/leetcode/editor/cn/Leetcode50PowxN.java
@@ -0,0 +1,16 @@
+package com.idevebi.leetcode.editor.cn;
+
+public class Leetcode50PowxN {
+    public static void main(String[] args) {
+        Solution solution = new Leetcode50PowxN().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public double myPow(double x, int n) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
