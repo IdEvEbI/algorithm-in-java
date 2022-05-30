13da741cb4e4ba2ddce2080b8cd252f9a731efeb
壹刀流 <idevebi@163.com>
2022-05-30 09:18:59 +0800

    refactor: leetcode [206] reverse linked list

---
+++ src/com/idevebi/leetcode/editor/cn/Leetcode206ReverseLinkedList.java
@@ -0,0 +1,39 @@
+package com.idevebi.leetcode.editor.cn;
+
+import com.idevebi.leetcode.editor.cn.datastructures.linkedList.ListNode;
+import com.idevebi.leetcode.editor.cn.util.LinkedListHelper;
+
+public class Leetcode206ReverseLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new Leetcode206ReverseLinkedList().new Solution();
+
+        ListNode head = LinkedListHelper.createTestList();
+
+        head = solution.reverseList(head);
+        System.out.println(head);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            // 翻转后的头指针
+            ListNode rHead = null;
+
+            // 当前头指针
+            ListNode cur = head;
+
+            while (cur != null) {
+                ListNode next = cur.next;
+
+                cur.next = rHead;
+                rHead = cur;
+                cur = next;
+            }
+
+            return rHead;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
