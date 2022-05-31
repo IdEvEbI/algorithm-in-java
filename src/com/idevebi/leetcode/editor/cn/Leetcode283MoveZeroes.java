a5c218bc9b819ced93eed231bd51da8c32a30e14
壹刀流 <idevebi@163.com>
2022-05-31 19:31:14 +0800

    refactor: leetcode [283] move zeroes

---
+++ src/com/idevebi/leetcode/editor/cn/Leetcode283MoveZeroes.java
@@ -0,0 +1,40 @@
+package com.idevebi.leetcode.editor.cn;
+
+import java.util.Arrays;
+
+public class Leetcode283MoveZeroes {
+    public static void main(String[] args) {
+        Solution solution = new Leetcode283MoveZeroes().new Solution();
+
+        int[] nums = {0, 1, 0, 3, 12};
+        solution.moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        /**
+         * 双指针，一个记录数组索引，一个记录非零元素索引
+         */
+        public void moveZeroes(int[] nums) {
+            // 记录非零元素索引
+            int idx = 0;
+
+            // 遍历数组
+            for (int i = 0; i < nums.length; i++) {
+                // 如果元素不为 0，将元素移动到 idx 位置，同时 idx++
+                if (nums[i] != 0) {
+
+                    if (i != idx) {
+                        nums[idx] = nums[i];
+                        nums[i] = 0;
+                    }
+
+                    idx++;
+                }
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
