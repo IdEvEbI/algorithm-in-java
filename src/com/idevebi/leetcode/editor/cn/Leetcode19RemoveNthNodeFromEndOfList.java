99c37660e90f0386dcb4916e39a105dfc9bb8af0
壹刀流 <idevebi@163.com>
2022-05-31 20:43:57 +0800

    refactor: leetcode [19] remove nth node from end of list

---
+++ src/com/idevebi/leetcode/editor/cn/Leetcode19RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,48 @@
+package com.idevebi.leetcode.editor.cn;
+
+import com.idevebi.leetcode.editor.cn.datastructures.linkedList.ListNode;
+import com.idevebi.leetcode.editor.cn.util.LinkedListHelper;
+
+public class Leetcode19RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        Solution solution = new Leetcode19RemoveNthNodeFromEndOfList().new Solution();
+
+        ListNode head = LinkedListHelper.createTestList();
+
+        head = solution.removeNthFromEnd(head, 2);
+        System.out.println(head);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            // 1. 增加哨兵节点
+            ListNode node = new ListNode(0);
+            node.next = head;
+
+            // 2. 计算链表长度
+            int len = getListLength(head);
+
+            // 3. 遍历链表找到倒数第 n 个结点的前置结点
+            ListNode cur = node;
+            for (int i = 0; i < len - n; i++) {
+                cur = cur.next;
+            }
+
+            cur.next = cur.next.next;
+
+            return node.next;
+        }
+
+        private int getListLength(ListNode head) {
+            int i = 0;
+            for (; head != null; i++) {
+                head = head.next;
+            }
+            return i;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
