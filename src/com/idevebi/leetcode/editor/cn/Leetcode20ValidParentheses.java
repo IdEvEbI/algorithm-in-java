518b51dfadeed6c99496ad45a930eb732097a274
壹刀流 <idevebi@163.com>
2022-05-31 10:19:32 +0800

    refactor: leetcode [20] valid parentheses

---
+++ src/com/idevebi/leetcode/editor/cn/Leetcode20ValidParentheses.java
@@ -0,0 +1,38 @@
+package com.idevebi.leetcode.editor.cn;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Leetcode20ValidParentheses {
+    public static void main(String[] args) {
+        Solution solution = new Leetcode20ValidParentheses().new Solution();
+
+        System.out.println(solution.isValid("()[]{}"));
+        System.out.println(solution.isValid("([)]"));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+
+            // 顺序遍历字符串中的字符
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    stack.push(')');
+                } else if (c == '[') {
+                    stack.push(']');
+                } else if (c == '{') {
+                    stack.push('}');
+                } else if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+
+            return stack.isEmpty();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
