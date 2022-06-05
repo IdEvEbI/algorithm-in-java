953de1b5ad36938335c14ae704d1f87b957e13cd
壹刀流 <idevebi@163.com>
2022-06-06 03:13:49 +0800

    feat: leetcode [589] N tree preorder traversal

--- src/com/idevebi/datastructures/tree/leetcode/editor/cn/NAryTreePreorderTraversal.java
+++ src/com/idevebi/datastructures/tree/leetcode/editor/cn/NAryTreePreorderTraversal.java
@@ -3,6 +3,7 @@ package com.idevebi.datastructures.tree.leetcode.editor.cn;
 import com.idevebi.datastructures.tree.Node;
 import com.idevebi.datastructures.util.NTreeHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,16 +14,39 @@ public class NAryTreePreorderTraversal {
         Solution solution = new NAryTreePreorderTraversal().new Solution();
 
         Node root = NTreeHelper.createTestNTree();
-        System.out.println(root);
+        List<Integer> list = solution.preorder(root);
+
+        System.out.println(list);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
 
     class Solution {
         public List<Integer> preorder(Node root) {
-            return null;
+            List<Integer> ans = new ArrayList<>();
+
+            dfs(root, ans);
+
+            return ans;
+        }
+
+        private void dfs(Node root, List<Integer> ans) {
+            if (root == null) {
+                return;
+            }
+
+            ans.add(root.val);
+
+            if (root.children == null) {
+                return;
+            }
+
+            for (Node n : root.children) {
+                dfs(n, ans);
+            }
         }
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
